c0ecbbda21d2f1973f997cc55d171eba9e594b16
Akram465 <ademak465@gmail.com>
2021-11-03 20:09:17 +0100

    table mode

--- instruments.php
+++ instruments.php
@@ -24,44 +24,66 @@ if ($conn) {
 }
 ?>
 
-<?php foreach ($instruments as $instrument) : ?>
+<?php foreach ($instruments as $instrument) :/* ?>
 
-    <hr>
+<hr>
 
 
-    <p>
+<p>
         <strong>name :</strong>
         <!-- Same thing as echo -->
         <?= $instrument['name']; ?>
-    </p>
-
+        </p>
+        <hr>
     <p>
-        <strong> brand_name':</strong>
-        <!-- Same thing as echo -->
-        <?= $instrument['brand_name']; ?>
+    <strong> brand_name':</strong>
+    <!-- Same thing as echo -->
+    <?= $instrument['brand_name']; ?>
     </p>
-
+    <hr>
     <p>
-        <strong>price :</strong>
-        <!-- Same thing as echo -->
-        <?= $instrument['price']; ?>
+    <strong>price :</strong>
+    <!-- Same thing as echo -->
+    <?= $instrument['price']; ?>
     </p>
+    <hr>
     <p>
         <strong>photo :</strong>
         <!-- Same thing as echo -->
         <?= $instrument['photo']; ?>
-    </p>
-    <p>
+        </p>
+        <hr>
+        <p>
         <strong>type :</strong>
         <!-- Same thing as echo -->
         <?= $instrument['type']; ?>
-    </p>
-    <p>
+        </p>
+        <hr>
+        <p>
         <strong>description :</strong>
         <!-- Same thing as echo -->
         <?= $instrument['description']; ?>
-    </p>
+        </p>
+        <hr>*/
+?>
 
 
-
+    <table border="1">
+        <tr>
+            <th>name</th>
+            <th>brand_name</th>
+            <th>price</th>
+            <th>$photo</th>
+            <th>type</th>
+            <th>description</th>
+        </tr>
+        <tr>
+            <td>$instrument['name']</td>
+            <td> <?= $instrument['brand_name']; ?></td>
+            <td> <?= $instrument['price']; ?></td>
+            <td><?= $instrument['photo']; ?></td>
+            <td><?= $instrument['type']; ?></td>
+            <td><?= $instrument['description']; ?></td>
+        </tr>
+    </table>
 <?php endforeach; ?>
